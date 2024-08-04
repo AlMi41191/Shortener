845dab6de696d387af69f27c0343c39881286091
Alexander Mikitas <almi41191@gmail.com>
2024-08-04 16:20:08 +0300

    Update classes SpeedTest, FunctionalTest and add junit-jupiter-params to pom.xml

--- src/test/java/org/example/FunctionalTest.java
+++ src/test/java/org/example/FunctionalTest.java
@@ -2,46 +2,30 @@ package org.example;
 
 import org.example.strategy.*;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FunctionalTest {
-    @Test
-    public void testHashMapStorageStrategy() {
-        Shortener shortener = new Shortener(new HashMapStorageStrategy());
-        testStorage(shortener);
+
+    @ValueSource
+    public Stream<StorageStrategy> streamListStorageStrategy() {
+        return Stream.of(new HashMapStorageStrategy(),
+                new HashBiMapStorageStrategy(),
+                new OurHashMapStorageStrategy(),
+                new OurHashBiMapStorageStrategy(),
+                new FileStorageStrategy());
     }
 
-    @Test
-    public void testOurHashMapStorageStrategy() {
-        Shortener shortener = new Shortener(new OurHashMapStorageStrategy());
-        testStorage(shortener);
-    }
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testStorage(StorageStrategy strategy) {
+        Shortener shortener = new Shortener(strategy);
 
-    @Test
-    public void testFileStorageStrategy() {
-        Shortener shortener = new Shortener(new FileStorageStrategy());
-        testStorage(shortener);
-    }
-
-    @Test
-    public void testHashBiMapStorageStrategy() {
-        Shortener shortener = new Shortener(new HashBiMapStorageStrategy());
-        testStorage(shortener);
-    }
-
-    @Test
-    public void testDualHashBidiMapStorageStrategy() {
-        Shortener shortener = new Shortener(new DualHashBidiMapStorageStrategy());
-        testStorage(shortener);
-    }
-
-    @Test
-    public void testOurHashBiMapStorageStrategy() {
-        Shortener shortener = new Shortener(new OurHashBiMapStorageStrategy());
-        testStorage(shortener);
-    }
-
-    public void testStorage(Shortener shortener) {
         String testString1 = "Test string 1";
         String testString2 = "Test string 2";
         String testString3 = "Test string 1";
@@ -57,6 +41,63 @@ public class FunctionalTest {
         Assertions.assertNotEquals(testId1, testId2);
         Assertions.assertNotEquals(testId2, testId3);
         Assertions.assertEquals(testId1, testId3);
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testContainsKey(StorageStrategy strategy) {
+        strategy.put(1L, "key1");
+        strategy.put(2L, "key2");
+
+        Assertions.assertTrue(strategy.containsKey(1L));
+        Assertions.assertTrue(strategy.containsKey(2L));
+        Assertions.assertFalse(strategy.containsKey(999L));
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testContainsValue(StorageStrategy strategy) {
+        strategy.put(1L, "key1");
+        strategy.put(2L, "key2");
+
+        Assertions.assertTrue(strategy.containsValue("key1"));
+        Assertions.assertTrue(strategy.containsValue("key2"));
+        Assertions.assertFalse(strategy.containsValue("999false"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testPut(StorageStrategy strategy) {
+        strategy.put(1L, "key1");
+        strategy.put(2L, "key2");
+
+        Assertions.assertEquals(1L, strategy.getKey("key1"));
+        Assertions.assertEquals(2L, strategy.getKey("key2"));
+
+        Assertions.assertEquals("key1", strategy.getValue(1L));
+        Assertions.assertEquals("key2", strategy.getValue(2L));
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testGetKey(StorageStrategy strategy) {
+        strategy.put(1L, "key1");
+        strategy.put(2L, "key2");
+
+        Assertions.assertEquals(1L, strategy.getKey("key1"));
+        Assertions.assertEquals(2L, strategy.getKey("key2"));
+        Assertions.assertNull(strategy.getKey("999false"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamListStorageStrategy")
+    public void testGetValue(StorageStrategy strategy) {
+        strategy.put(1L, "key1");
+        strategy.put(2L, "key2");
+
+        Assertions.assertEquals("key1", strategy.getValue(1L));
+        Assertions.assertEquals("key2", strategy.getValue(2L));
+        Assertions.assertNull(strategy.getValue(999L));
     }
 }
 
