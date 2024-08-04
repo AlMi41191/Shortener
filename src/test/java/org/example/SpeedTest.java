97031cd5566697ff858dac493493a5d2d35c424e
Alexander <134769734+AlMi41191@users.noreply.github.com>
2024-08-04 15:37:35 +0300

    Update SpeedTest.java

--- src/test/java/org/example/SpeedTest.java
+++ src/test/java/org/example/SpeedTest.java
@@ -7,10 +7,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SpeedTest {
-    public long getTimeToGetIds(Shortener shortener, Set<String> strings, Set<Long> ids) {
-        return 0;
-    }
-
     @Test
     public void testHashMapStorage() {
         Shortener shortener1 = new Shortener(new HashMapStorageStrategy());
@@ -18,14 +14,37 @@ public class SpeedTest {
 
         Set<String> origStrings = new HashSet<>();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10000; ++i) {
             origStrings.add(Helper.generateRandomString());
         }
 
-        Set<Long> longIds = new HashSet<>();
+        Set<Long> ids1 = new HashSet<>();
+        Set<String> strings1 = new HashSet<>();
+        long stringToIdTime1 = getTimeToGetIds(shortener1, origStrings, ids1);
+        long idToStringTime1 = getTimeToGetStrings(shortener1, ids1, strings1);
 
-        for (int i = 0; i < 10000; i++) {
-            origStrings.add(Helper.generateRandomString());
-        }
+        Set<Long> ids2 = new HashSet<>();
+        Set<String> strings2 = new HashSet<>();
+        long stringToIdTime2 = getTimeToGetIds(shortener2, origStrings, ids2);
+        long idToStringTime2 = getTimeToGetStrings(shortener2, ids2, strings2);
+
+        Assert.assertTrue(stringToIdTime1 > stringToIdTime2);
+        Assert.assertEquals(idToStringTime1, idToStringTime2, 30);
+    }
+
+    public long getTimeToGetIds(Shortener shortener, Set<String> strings, Set<Long> ids) {
+        Date startTimestamp = new Date();
+        for (String s : strings)
+            ids.add(shortener.getId(s));
+        Date endTimestamp = new Date();
+        return endTimestamp.getTime() - startTimestamp.getTime();
+    }
+
+    public long getTimeToGetStrings(Shortener shortener, Set<Long> ids, Set<String> strings) {
+        Date startTimestamp = new Date();
+        for (Long id : ids)
+            strings.add(shortener.getString(id));
+        Date endTimestamp = new Date();
+        return endTimestamp.getTime() - startTimestamp.getTime();
     }
 }
